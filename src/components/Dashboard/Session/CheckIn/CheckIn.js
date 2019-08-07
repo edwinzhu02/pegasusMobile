7e84c14295b3715bd22678db5fe66a51e860c1d6
tee1365 <31180529+tee1365@users.noreply.github.com>
2019-08-07 15:39:27 +1200

    update

--- src/components/Dashboard/Session/CheckIn/CheckIn.js
+++ src/components/Dashboard/Session/CheckIn/CheckIn.js
@@ -1,209 +1,221 @@
-import React, {Component} from 'react'
-import {View,StyleSheet,Text,TouchableOpacity,FlatList} from 'react-native'
-import {ListItem} from 'react-native-elements'
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  Alert
+} from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import { ListItem } from "react-native-elements";
 
-export default class CheckIn extends Component{
-    static navigationOptions = {
-        title: 'Check In'
-    };
+//http://gradspace.org:5000/api/loginlog/CheckIn => userId,locationX,locationY
 
-    state={
-        dateTime: "",
-        recentRecordTime: '13:13',
-        location:{
-            ready: false,
-            where: {lat:null,lng:null},
-            error: null
-        },
-        //这个是模拟数据 （打卡历史记录）
-        history: [
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-10 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-11 13:11:11'},
-            {logType: "Check in", createAt: '2019-04-12 13:19:11'},
-            {logType: "Check in", createAt: '2019-04-13 13:19:11'},
+export default class CheckIn extends Component {
+  static navigationOptions = {
+    title: "Check In"
+  };
 
-        ]
-    }
-    componentDidMount = () =>
-    {
-        //get Date time now
-        var that = this;
-        var date = new Date().getDate(); //Current Date
-        var month = new Date().getMonth() + 1; //Current Month
-        var year = new Date().getFullYear(); //Current Year
-        var hours = new Date().getHours(); //Current Hours
-        var min = new Date().getMinutes(); //Current Minutes
-        that.setState({
-            //Setting the value of the date time
-            dateTime:
-                date + '/' + month + '/' + year + ' ' + hours + ':' + min
-        });
+  CHECK_IN_API = "http://gradspace.org:5000/api/loginlog/CheckIn";
 
-        //get location by gps
-        let geoOptions = {
-            enableHighAccuracy: true,
-            timeout: 20000,
-            maximumAge: 60 * 60 * 24
-        }
-        this.setState({location:{ready: false}})
-        navigator.geolocation.getCurrentPosition(
-            this.geoSuccess,this.goFailure,geoOptions
-        )
-    }
+  state = {
+    userId: "",
+    dateTime: "",
+    recentRecordTime: "13:13",
+    location: {
+      ready: false,
+      where: { lat: null, lng: null },
+      error: null
+    },
+    history: []
+  };
 
-    geoSuccess = (position) => {
-        this.setState({
-            location:{
-                ready: true,
-                where: {lat: position.coords.latitude,lng:position.coords.longitude}
-            }
+  componentWillMount = async () => {
+    this.setState({ userId: await AsyncStorage.getItem("userid") }, () => {
+      fetch(`http://gradspace.org:5000/api/loginlog/${this.state.userId}`)
+        .then(response => {
+          console.log(response, this.state.userId);
+          return response.json();
         })
-    }
+        .then(result =>
+          this.setState({ history: result.Data }, () => console.log(this.state))
+        )
+        .catch(error => console.log(error));
+    });
+  };
 
-    goFailure = (err) => {
-        this.setState({location:{error: err.message}})
-    }
+  componentDidMount = () => {
+    //get Date time now
+    var that = this;
+    var date = new Date().getDate(); //Current Date
+    var month = new Date().getMonth() + 1; //Current Month
+    var year = new Date().getFullYear(); //Current Year
+    var hours = new Date().getHours(); //Current Hours
+    var min = new Date().getMinutes(); //Current Minutes
+    that.setState({
+      //Setting the value of the date time
+      dateTime: date + "/" + month + "/" + year + " " + hours + ":" + min
+    });
 
-    CheckInHandler = () => {
-        console.log(this.state)
-    }
+    //get location by gps
+    let geoOptions = {
+      enableHighAccuracy: true,
+      timeout: 20000,
+      maximumAge: 60 * 60 * 24
+    };
+    this.setState({ location: { ready: false } });
+    navigator.geolocation.getCurrentPosition(
+      this.geoSuccess,
+      this.goFailure,
+      geoOptions
+    );
+  };
 
-    renderSeparator = () =>{
-        return (
-            <View
-                style={{
-                    height:1,
-                    backgroundColor: '#CED0CE',
-                }}
+  geoSuccess = position => {
+    this.setState({
+      location: {
+        ready: true,
+        where: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+      }
+    });
+  };
+
+  goFailure = err => {
+    this.setState({ location: { error: err.message } });
+  };
+
+  CheckInHandler = () => {
+    let body = JSON.stringify({
+      UserId: this.state.userId,
+      LocaltionX: this.state.location.where.lat,
+      LocaltionY: this.state.location.where.lng
+    });
+
+    console.log(body);
+
+    fetch(this.CHECK_IN_API, {
+      method: "POST",
+      body: body,
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(result => Alert.alert("", result.Data, [{ text: "OK" }]))
+      .catch(err => console.log(err));
+  };
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          backgroundColor: "#CED0CE"
+        }}
+      />
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.topShadow}>
+          <Text style={styles.dateText}>{this.state.dateTime}</Text>
+          <Text style={styles.recentRecord}>
+            Record for today check in: {this.state.recentRecordTime}
+          </Text>
+          <View style={styles.CircleContainer}>
+            <TouchableOpacity onPress={() => this.CheckInHandler()}>
+              <View style={styles.CircleButton}>
+                <Text style={styles.ButtonText}>Check In</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <Text style={{ padding: 5, color: "white" }}>
+            签到 09:03 迟到 3 分钟
+          </Text>
+          <Text style={{ padding: 5, color: "white" }}>
+            工作时间 09:00-18:00
+          </Text>
+        </View>
+        <View style={styles.history}>
+          {this.state.history.length > 0 ? (
+            <FlatList
+              data={this.state.history.map(el => {
+                if (el.LogType == 1) {
+                  el.LogType = "Check In";
+                } else {
+                  el.LogType = "Check Out";
+                }
+                return el;
+              })}
+              renderItem={({ item }) => {
+                console.log(item);
+                return (
+                  <ListItem
+                    title={`${item.LogType}`}
+                    subtitle={`${item.CreatedAt.replace("T", " ")}`}
+                    rightTitle={`${item.Abbr}`}
+                  />
+                );
+              }}
+              ItemSeparatorComponent={this.renderSeparator}
+              keyExtractor={(item, index) => index.toString()}
             />
-        )
-    }
-
-    render(){
-        return(
-            <View style={styles.container}>
-                <View style={styles.topShadow}>
-                    <Text style={styles.dateText}>
-                        {this.state.dateTime}
-                    </Text>
-                    <Text style={styles.recentRecord}>
-                        Record for today check in: {this.state.recentRecordTime}
-                    </Text>
-                    <View style={styles.CircleContainer}>
-                        <TouchableOpacity onPress={()=>this.CheckInHandler()}>
-                            <View style={styles.CircleButton}>
-                                <Text style={styles.ButtonText}>Check In</Text>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
-                    <Text style={{padding: 5,color:'white'}}>
-                        签到 09:03 迟到 3 分钟
-                    </Text>
-                    <Text style={{padding:5,color:'white'}}>
-                        工作时间 09:00-18:00
-                    </Text>
-                </View>
-                <View style={styles.history}>
-                    {this.state.history.length > 0?
-                        <FlatList
-                            data={this.state.history}
-                            renderItem={({item})=>(
-                                <ListItem
-                                    title={`${item.logType}`}
-                                    subtitle={`${item.createAt}`}
-                                />
-                            )
-                            }
-                            ItemSeparatorComponent={this.renderSeparator}
-                            keyExtractor={(item, index) => index.toString()}
-                        />
-                        :
-                        <Text style={{alignSelf: 'center',fontWeight: 'bold',fontSize: 25}}>No data</Text>
-                    }
-                </View>
-            </View>
-        )
-    }
+          ) : (
+            <Text
+              style={{ alignSelf: "center", fontWeight: "bold", fontSize: 25 }}
+            >
+              No data
+            </Text>
+          )}
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-    },
-    topShadow:{
-        flex:4,
-        alignItems: 'center',
-        backgroundColor: 'rgb(34,143,198)'
-    },
-    dateText:{
-        paddingTop: 20,
-        fontSize: 18,
-        color: 'white'
-    },
-    recentRecord:{
-        color: 'white',
-        paddingTop: 10,
-        fontSize: 15,
-    },
-    CircleButton:{
-        height: 150,
-        width: 150,
-        borderRadius:300,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
-
-    },
-    CircleContainer:{
-        justifyContent: 'center',
-        paddingVertical: 15,
-        alignItems: 'center',
-    },
-    ButtonText:{
-        fontSize: 20,
-    },
-    history:{
-        flex:3,
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flex: 1
+  },
+  topShadow: {
+    flex: 4,
+    alignItems: "center",
+    backgroundColor: "rgb(34,143,198)"
+  },
+  dateText: {
+    paddingTop: 20,
+    fontSize: 18,
+    color: "white"
+  },
+  recentRecord: {
+    color: "white",
+    paddingTop: 10,
+    fontSize: 15
+  },
+  CircleButton: {
+    height: 150,
+    width: 150,
+    borderRadius: 300,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  CircleContainer: {
+    justifyContent: "center",
+    paddingVertical: 15,
+    alignItems: "center"
+  },
+  ButtonText: {
+    fontSize: 20
+  },
+  history: {
+    flex: 3,
+    justifyContent: "center"
+  }
+});
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -39,11 +39,15 @@ export default class Login extends Component {
         return res.json();
       })
       .then(async result => {
+        console.log(result);
         if (result.IsSuccess == false) {
           throw new Error(result.ErrorMessage);
         }
         await AsyncStorage.setItem("token", result.Data.token);
-        await AsyncStorage.setItem("expires", result.Data.expires);
+        await AsyncStorage.setItem(
+          "expires",
+          JSON.stringify(result.Data.expires)
+        );
         await AsyncStorage.setItem(
           "firstname",
           result.Data.userdetails.firstname
@@ -51,6 +55,14 @@ export default class Login extends Component {
         await AsyncStorage.setItem(
           "lastname",
           result.Data.userdetails.lastname
+        );
+        await AsyncStorage.setItem(
+          "OrgId",
+          JSON.stringify(result.Data.userdetails.OrgId)
+        );
+        await AsyncStorage.setItem(
+          "userid",
+          JSON.stringify(result.Data.userid)
         );
         this.setState({ visible: false });
         this.props.navigation.navigate("App");
@@ -79,26 +91,28 @@ export default class Login extends Component {
           </View>
           <View style={{ minHeight: 185 }}>
             {Platform.OS === "ios" ? (
-                <View style={{minHeight: 185}}>
-                    <RkTextInput
-                        onChangeText={text=>this.setState({username: text})}
-                        rkType='underline topLabel'
-                        label='EMAIL ADDRESS'
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        labelStyle={styles.inputLabel}
-                        containerStyle={styles.inputContainer}
-                        style={styles.input}/>
-                    <RkTextInput
-                        rkType='underline topLabel'
-                        onChangeText={text=>this.setState({password: text})}
-                        label='PASSWORD'
-                        labelStyle={styles.inputLabel}
-                        containerStyle={styles.inputContainer}
-                        style={styles.input}
-                        secureTextEntry={true}/>
-                </View>
+              <View style={{ minHeight: 185 }}>
+                <RkTextInput
+                  onChangeText={text => this.setState({ username: text })}
+                  rkType="underline topLabel"
+                  label="EMAIL ADDRESS"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  labelStyle={styles.inputLabel}
+                  containerStyle={styles.inputContainer}
+                  style={styles.input}
+                />
+                <RkTextInput
+                  rkType="underline topLabel"
+                  onChangeText={text => this.setState({ password: text })}
+                  label="PASSWORD"
+                  labelStyle={styles.inputLabel}
+                  containerStyle={styles.inputContainer}
+                  style={styles.input}
+                  secureTextEntry={true}
+                />
+              </View>
             ) : (
               <View>
                 <TextInput
