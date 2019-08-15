1e3be3d56fb48c1794fc55de751f6d304646c7fb
tee1365 <31180529+tee1365@users.noreply.github.com>
2019-08-15 13:46:06 +1200

    update

--- src/components/Dashboard/Session/Feedback/Feedback.js
+++ src/components/Dashboard/Session/Feedback/Feedback.js
@@ -4,81 +4,95 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  FlatList, Alert
+  FlatList,
+  Alert
 } from "react-native";
 import { ListItem } from "react-native-elements";
-import '../../../../util/global_config'
+import "../../../../util/global_config";
 import AsyncStorage from "@react-native-community/async-storage";
-import {ActivityIndicator, Colors} from "react-native-paper";
-import {connect} from 'react-redux';
-import { Icon } from 'react-native-elements'
+import { ActivityIndicator, Colors } from "react-native-paper";
+import { connect } from "react-redux";
+import { Icon } from "react-native-elements";
 class Feedback extends Component {
   static navigationOptions = {
     title: "Feedback"
   };
 
-  constructor(props){
-      super(props)
-      this.state = {
-          isLoaded: false,
-          lessons: [],
-          userId: "",
-          userPosition: ""
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      lessons: [],
+      userId: "",
+      userPosition: ""
+    };
   }
 
-  componentWillReceiveProps = ()=> {
-      this.getDataHandler()
-  }
+  componentWillReceiveProps = () => {
+    this.getDataHandler();
+  };
 
-    componentDidMount = async ()=>{
-    this.setState({
-      userId: await AsyncStorage.getItem("userid"),
-      userPosition: await AsyncStorage.getItem("userPosition")
-    },()=>{
-      this.getDataHandler()
-    })
-  }
-    getDataHandler = () => {
-    if (this.state.userPosition == "teacher"){
-      fetch(global.constants.basic_url + 'rating/TeacherFeedbackRatingList/' + this.state.userId)
-          .then(res=>res.json())
-          .then(result=>{
-              if (result.IsSuccess == false) {
-                throw new Error(result.ErrorMessage);
-              }
-              this.setState({
-                  lessons: result.Data.sort((a,b)=>{
-                      if (a.isRate >= b.isRate) return 1
-                      return -1
-                  }),
-                  isLoaded:true
-              })
-            }).catch(err=> {
-                Alert.alert("Data loading Fail", err.toString())
-      })
-    }else if (this.state.userPosition == "learner"){
-        fetch(global.constants.basic_url + 'rating/LearnerFeedbackRatingList/' + this.state.userId)
-            .then(res=>res.json())
-            .then(result=>{
-                if (result.IsSuccess == false) {
-                    throw new Error(result.ErrorMessage);
-                }
-
-                this.setState({
-                    lessons: result.Data.sort((a,b)=>{
-                        if (a.isRate > b.isRate) return 1
-                        return -1
-                    }),
-                    isLoaded:true
-                })
-            }).catch(err=>{
-                Alert.alert("Data loading Fail", err.toString())
+  componentDidMount = async () => {
+    this.setState(
+      {
+        userId: await AsyncStorage.getItem("userid"),
+        userPosition: await AsyncStorage.getItem("userPosition")
+      },
+      () => {
+        this.getDataHandler();
+      }
+    );
+  };
+  getDataHandler = () => {
+    if (this.state.userPosition == "teacher") {
+      fetch(
+        global.constants.basic_url +
+          "rating/TeacherFeedbackRatingList/" +
+          this.state.userId
+      )
+        .then(res => res.json())
+        .then(result => {
+          if (result.IsSuccess == false) {
+            throw new Error(result.ErrorMessage);
+          }
+          this.setState({
+            lessons: result.Data.sort((a, b) => {
+              if (a.isRate >= b.isRate) return 1;
+              return -1;
+            }),
+            isLoaded: true
+          });
         })
-    }
-  }
+        .catch(err => {
+          Alert.alert("Data loading Fail", err.toString());
+        });
+    } else if (this.state.userPosition == "learner") {
+      fetch(
+        global.constants.basic_url +
+          "rating/LearnerFeedbackRatingList/" +
+          this.state.userId
+      )
+        .then(res => res.json())
+        .then(result => {
+          if (result.IsSuccess == false) {
+            throw new Error(result.ErrorMessage);
+          }
 
-    renderSeparator = () => {
+          this.setState({
+            lessons: result.Data.sort((a, b) => {
+              if (a.isRate > b.isRate) return 1;
+              return -1;
+            }),
+            isLoaded: true
+          });
+        })
+        .catch(err => {
+          Alert.alert("Data loading Fail", err.toString());
+        });
+    }
+  };
+
+  renderSeparator = () => {
     return (
       <View
         style={{
@@ -90,39 +104,44 @@ class Feedback extends Component {
   };
 
   render() {
-    return (
-        this.state.isLoaded?(<View style={styles.container}>
-          {this.state.lessons.length > 0 ? (
-              <FlatList
-                  data={this.state.lessons}
-                  renderItem={({ item }) => (
-                      <ListItem
-                          rightIcon={{ name: "chevron-right" }}
-                          leftIcon={item.isRate==0?(<Icon name='more-vert' color='red'/>):(<Icon name='check' color='green'/>)}
-                          title={`${item.CourseName}`}
-                          subtitle={`${item.BeginTime.replace('T', ' ')}`}
-                          onPress={() => this.props.navigation.navigate("FeedbackRating",{
-                            LessonId: item.LessonId,
-                            role: this.state.userPosition,
-                            userId: this.state.userId,
-                            isRate: item.isRate
-                          })}
-                      />
-                  )}
-                  ItemSeparatorComponent={this.renderSeparator}
-                  keyExtractor={(item, index) => index.toString()}
+    return this.state.isLoaded ? (
+      <View style={styles.container}>
+        {this.state.lessons.length > 0 ? (
+          <FlatList
+            data={this.state.lessons}
+            renderItem={({ item }) => (
+              <ListItem
+                rightIcon={{ name: "chevron-right" }}
+                leftIcon={
+                  item.isRate == 0 ? (
+                    <Icon name="more-vert" color="red" />
+                  ) : (
+                    <Icon name="check" color="green" />
+                  )
+                }
+                title={`${item.CourseName}`}
+                subtitle={`${item.BeginTime.replace("T", " ")}`}
+                onPress={() =>
+                  this.props.navigation.navigate("FeedbackRating", {
+                    LessonId: item.LessonId,
+                    role: this.state.userPosition,
+                    userId: this.state.userId,
+                    isRate: item.isRate
+                  })
+                }
               />
-          ) : (
-              <Text>No Data</Text>
-          )}
-        </View>):(
-            <View style={{flex:1,justifyContent: 'center', alignItems:'center'}}>
-              <ActivityIndicator
-                  size={50}
-                  color={Colors.blue100}
-              />
-            </View>
-        )
+            )}
+            ItemSeparatorComponent={this.renderSeparator}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        ) : (
+          <Text>No Data</Text>
+        )}
+      </View>
+    ) : (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size={50} color={Colors.blue100} />
+      </View>
     );
   }
 }
@@ -133,9 +152,8 @@ const styles = StyleSheet.create({
   }
 });
 
-const mapStateToProps = (state) =>({
-    IsConfirm: state.CommentConfirm
-})
+const mapStateToProps = state => ({
+  IsConfirm: state.CommentConfirm
+});
 
-
-export default connect(mapStateToProps)(Feedback)
+export default connect(mapStateToProps)(Feedback);
--- src/components/Dashboard/Session/FeedbackView/FeedbackView.js
+++ src/components/Dashboard/Session/FeedbackView/FeedbackView.js
@@ -1,193 +1,226 @@
-import React, {Component} from 'react'
-import {View,StyleSheet,Text,ScrollView,Alert} from 'react-native'
-import FeedbackViewItem from './FeedbackViewItem/FeedbackViewItem'
-import Star from 'react-native-star-view'
-import {ActivityIndicator, Colors} from "react-native-paper";
-import '../../../../util/global_config'
+import React, { Component } from "react";
+import { View, StyleSheet, Text, ScrollView, Alert } from "react-native";
+import FeedbackViewItem from "./FeedbackViewItem/FeedbackViewItem";
+import Star from "react-native-star-view";
+import { ActivityIndicator, Colors } from "react-native-paper";
+import "../../../../util/global_config";
 import AsyncStorage from "@react-native-community/async-storage";
-import '../../../../util/FeedbackStatistics'
-import {statistics} from "../../../../util/FeedbackStatistics";
-export default class FeedbackView extends Component{
-    static navigationOptions = {
-        title: 'Feedback View'
+export default class FeedbackView extends Component {
+  static navigationOptions = {
+    title: "Feedback View"
+  };
+
+  constructor(props) {
+    super(props);
+    this.itemToRender = 0;
+    this.state = {
+      userId: "",
+      userPosition: "",
+      lessons: [],
+      statistics: {
+        average: 0,
+        star: { one: 0, two: 0, three: 0, four: 0, five: 0 }
+      },
+      itemToRender: this.itemToRender,
+      isShowLoadingIcon: false,
+      page: 0
     };
+  }
 
-    constructor(props){
-        super(props)
-        this.itemToRender = 5
-        this.state = {
-            userId: '',
-            userPosition: "",
-            lessons: [],
-            statistics: {
-                average: 0,
-                star: {one:0,two:0,three:0,four:0,five:0}
-            },
-            itemToRender: this.itemToRender,
-            isShowLoadingIcon: true
+  componentDidMount = async () => {
+    this.setState(
+      {
+        userId: await AsyncStorage.getItem("userid"),
+        userPosition: await AsyncStorage.getItem("userPosition")
+      },
+      () => {
+        this.fetchRating(this.state.userPosition, this.state.page);
+        this.fetchStatistics(this.state.userPosition, this.state.userId);
+      }
+    );
+  };
+
+  fetchStatistics(userPosition, userId) {
+    if (userPosition === "learner") {
+      url = "rating/GetStatisticsForLearner/";
+    } else if (userPosition === "teacher") {
+      url = "rating/GetStatisticsForTeacher/";
+    }
+
+    fetch(global.constants.basic_url + url + userId)
+      .then(res => res.json())
+      .then(result => {
+        this.setState({ statistics: result["Data"] });
+      });
+  }
+
+  fetchRating(userPosition, page) {
+    let url;
+    if (userPosition === "learner") {
+      url = "rating/LearnerGetRating/";
+    } else if (userPosition === "teacher") {
+      url = "rating/TeacherGetRating/";
+    }
+
+    fetch(global.constants.basic_url + url + this.state.userId + "/" + page)
+      .then(res => res.json())
+      .then(result => {
+        if (result.IsSuccess == false) {
+          throw new Error(result.ErrorMessage);
         }
-    }
-
-
-    componentDidMount = async () => {
+        let data = result.Data.map(s => {
+          return {
+            name: s.FirstName + " " + s.LastName,
+            rating: s.RateStar,
+            lessonStartDate: s.BeginTime,
+            comment: s.Comment
+          };
+        });
+        let newArr = this.state.lessons;
+        newArr.push(...data);
+        console.log(newArr);
+        console.log(page);
         this.setState({
-            userId: await AsyncStorage.getItem("userid"),
-            userPosition: await AsyncStorage.getItem("userPosition")
-        },()=>{
-            if (this.state.userPosition == "teacher"){
-                fetch(global.constants.basic_url + 'rating/TeacherGetRating/' + this.state.userId)
-                    .then(res=>res.json())
-                    .then(result=>{
-                        if (result.IsSuccess == false) {
-                            throw new Error(result.ErrorMessage);
-                        }
-                        const statistic = statistics(result.Data)
-                        let data = result.Data.map(s=>{
-                            return {name: s.FirstName+" "+s.LastName, rating: s.RateStar,lessonStartDate:s.BeginTime,comment: s.Comment}
-                        })
-                        this.setState({lessons:data, statistics: statistic})
-                    }).catch(err=>{
-                    Alert.alert(Alert.alert("Data loading Fail", err.toString()))
-                })
-            }else if (this.state.userPosition == "learner"){
-                console.log("sbc")
-                fetch(global.constants.basic_url + 'rating/LearnerGetRating/' + this.state.userId)
-                    .then(res=>res.json())
-                    .then(result=>{
-                        if (result.IsSuccess == false) {
-                            throw new Error(result.ErrorMessage);
-                        }
-                        const statistic = statistics(result.Data)
-                        let data = result.Data.map(s=>{
-                            return {name: s.FirstName+" "+s.LastName, rating: s.RateStar,lessonStartDate:s.BeginTime,comment: s.Comment}
-                        })
-                        this.setState({lessons:data, statistics: statistic})
-                    }).catch(err=>{
-                    Alert.alert(Alert.alert("Data loading Fail", err.toString()))
-                })
-            }
+          lessons: newArr,
+          page: this.state.page + 1,
+          itemToRender: this.state.itemToRender + 10
+        });
+      })
+      .catch(err => {
+        Alert.alert(Alert.alert("Data loading Fail", err.toString()));
+      });
+  }
 
-        })
-
-    }
-
-    render(){
-        const items = this.state.lessons.map((item,i)=>{
-            if (i +1 <=this.state.itemToRender){
-                return (
-                    <FeedbackViewItem
-                        name={item.name}
-                        lessonStartDate={item.lessonStartDate}
-                        comment={item.comment}
-                        key = {i}
-                        rating={item.rating}
-                    />
-                )
-            }
-        })
-
-
+  render() {
+    const items = this.state.lessons.map((item, i) => {
+      if (i + 1 <= this.state.itemToRender) {
         return (
-            <View style={styles.container}>
-                <View style={styles.topContainer}>
-                    <View style={styles.ratingText}>
-                        <Text style={{fontSize: 45}}>{this.state.statistics.average} of 5</Text>
-                    </View>
-                    <View>
-                        <View style={styles.ratingStar}>
-                            <View>
-                                <Star style={styles.star} score={1}/>
-                            </View>
-                            <View>
-                                <Text>({this.state.statistics.star.one})</Text>
-                            </View>
-                        </View>
-                        <View style={styles.ratingStar}>
-                            <View>
-                                <Star style={styles.star} score={2}/>
-                            </View>
-                            <View>
-                                <Text>({this.state.statistics.star.two})</Text>
-                            </View>
-                        </View>
-                        <View style={styles.ratingStar}>
-                            <View>
-                                <Star style={styles.star} score={3}/>
-                            </View>
-                            <View>
-                                <Text>({this.state.statistics.star.three})</Text>
-                            </View>
-                        </View>
-                        <View style={styles.ratingStar}>
-                            <View>
-                                <Star style={styles.star} score={4}/>
-                            </View>
-                            <View>
-                                <Text>({this.state.statistics.star.four})</Text>
-                            </View>
-                        </View>
-                        <View style={styles.ratingStar}>
-                            <View>
-                                <Star style={styles.star} score={5}/>
-                            </View>
-                            <View>
-                                <Text>({this.state.statistics.star.five})</Text>
-                            </View>
-                        </View>
-                    </View>
-                </View>
-                <View
-                    style={{
-                        height:1,
-                        backgroundColor: '#CED0CE',
-                        alignSelf: 'center'
-                    }}
-                />
-                <ScrollView
-                    onMomentumScrollEnd={(e)=>{
-                        const scrollPosition = e.nativeEvent.contentOffset.y;
-                        const scrollViewHeight = e.nativeEvent.layoutMeasurement.height
-                        const contentHeight = e.nativeEvent.contentSize.height
-                        const isScrolledToBottom = scrollViewHeight + scrollPosition;
+          <FeedbackViewItem
+            name={item.name}
+            lessonStartDate={item.lessonStartDate}
+            comment={item.comment}
+            key={i}
+            rating={item.rating}
+          />
+        );
+      }
+    });
 
-                        if (isScrolledToBottom >= (contentHeight-50)&&this.state.itemToRender<=this.state.lessons.length){
-                            this.setState({
-                                itemToRender: this.state.itemToRender + 5
-                            })
-                        }
-                        if (this.state.itemToRender >= this.state.lessons.length){
-                            this.setState({
-                                isShowLoadingIcon: false
-                            })
-                        }
-                    }}
-                >
-                    {items}
-                    <ActivityIndicator size={30} animating={this.state.isShowLoadingIcon} color={Colors.blue200} />
-                </ScrollView>
+    console.log(this.state.statistics);
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <View style={styles.ratingText}>
+            <Text style={{ fontSize: 45 }}>
+              {this.state.statistics.average} of 5
+            </Text>
+          </View>
+          <View>
+            <View style={styles.ratingStar}>
+              <View>
+                <Star style={styles.star} score={1} />
+              </View>
+              <View>
+                <Text>({this.state.statistics.star.one})</Text>
+              </View>
             </View>
-        )
-    }
+            <View style={styles.ratingStar}>
+              <View>
+                <Star style={styles.star} score={2} />
+              </View>
+              <View>
+                <Text>({this.state.statistics.star.two})</Text>
+              </View>
+            </View>
+            <View style={styles.ratingStar}>
+              <View>
+                <Star style={styles.star} score={3} />
+              </View>
+              <View>
+                <Text>({this.state.statistics.star.three})</Text>
+              </View>
+            </View>
+            <View style={styles.ratingStar}>
+              <View>
+                <Star style={styles.star} score={4} />
+              </View>
+              <View>
+                <Text>({this.state.statistics.star.four})</Text>
+              </View>
+            </View>
+            <View style={styles.ratingStar}>
+              <View>
+                <Star style={styles.star} score={5} />
+              </View>
+              <View>
+                <Text>({this.state.statistics.star.five})</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            height: 1,
+            backgroundColor: "#CED0CE",
+            alignSelf: "center"
+          }}
+        />
+        <ScrollView
+          onMomentumScrollEnd={e => {
+            const scrollPosition = e.nativeEvent.contentOffset.y;
+            const scrollViewHeight = e.nativeEvent.layoutMeasurement.height;
+            const contentHeight = e.nativeEvent.contentSize.height;
+            const isScrolledToBottom = scrollViewHeight + scrollPosition;
+            this.setState({ isShowLoadingIcon: true });
+            if (
+              isScrolledToBottom >= contentHeight - 50 &&
+              this.state.itemToRender <= this.state.lessons.length
+            ) {
+              this.fetchRating(this.state.userPosition, this.state.page);
+            }
+            if (this.state.itemToRender >= this.state.lessons.length) {
+              this.setState({
+                isShowLoadingIcon: false
+              });
+            }
+          }}
+        >
+          {items}
+
+          <ActivityIndicator
+            size={30}
+            animating={this.state.isShowLoadingIcon}
+            color={Colors.blue200}
+          />
+
+          {this.state.itemToRender > this.state.lessons.length ? (
+            <Text style={{ alignSelf: "center" }}>No remaining data</Text>
+          ) : null}
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        padding: 15
-    },
-    star:{
-        width: 100,
-        height: 20
-    },
-    topContainer:{
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    },
-    ratingText:{
-        justifyContent: 'center'
-    },
-    ratingStar:{
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    }
-})
+  container: {
+    flex: 1,
+    padding: 15
+  },
+  star: {
+    width: 100,
+    height: 20
+  },
+  topContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  ratingText: {
+    justifyContent: "center"
+  },
+  ratingStar: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  }
+});
