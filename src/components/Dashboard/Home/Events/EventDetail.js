09af2ab7b79c5ca1e9e80c050589539108ea707a
tee1365 <31180529+tee1365@users.noreply.github.com>
2019-08-20 10:44:22 +1200

    update

--- src/components/Dashboard/Home/Events/EventDetail.js
+++ src/components/Dashboard/Home/Events/EventDetail.js
@@ -6,32 +6,6 @@ const EventDetail = props => {
   let data = props.navigation.state.params.data;
   let width = Dimensions.get("window").width;
   return (
-    // <View style={{ flex: 1, alignItems: "center" }}>
-    //   <Title
-    //     style={{ marginTop: 10, marginLeft: 15, marginRight: 15, fontSize: 20 }}
-    //   >
-    //     {data.title}
-    //   </Title>
-    //   <Image
-    //     source={{ uri: data.url }}
-    //     style={{
-    //       width: width,
-    //       height: width * 0.618,
-    //       marginTop: 20,
-    //       marginBottom: 20
-    //     }}
-    //   />
-    //   <Paragraph
-    //     style={{
-    //       alignSelf: "flex-start",
-    //       marginLeft: 15,
-    //       marginRight: 15,
-    //       fontSize: 16
-    //     }}
-    //   >
-    //     {data.content}
-    //   </Paragraph>
-    // </View>
     <WebView
       source={{ uri: "https://github.com/facebook/react-native" }}
       style={{ marginTop: 20 }}
